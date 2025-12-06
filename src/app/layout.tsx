72be9886c0b068b90a63754914d26a832c2f93f1
Prodmse <prodmse3@gmail.com>
2025-12-06 16:26:16 +0000

    still not working and showing site icon favicon bro not solved check out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/favicon.png" sizes="any" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
       </head>
       <body className={poppins.className}>
           <TooltipProvider>
