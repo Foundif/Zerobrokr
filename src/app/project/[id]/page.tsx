e24cf11f9ef9287c987508707bf331f38318068e
Prodmse <prodmse3@gmail.com>
2025-12-06 14:17:59 +0000

    make header notice loops continuosly and reduce little speed slower also

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
 import { ArrowLeft, MapPin, Bed, Bath, Square, Calendar, CheckCircle, Phone } from 'lucide-react';
