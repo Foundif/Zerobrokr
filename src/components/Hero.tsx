0ebaa171b93157aca34467b9aae9cceaaa66f529
Foundif <foundifinnovations@gmail.com>
2026-01-02 11:25:35 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -129,7 +129,7 @@ const Hero = () => {
           {images.map((img, index) => (
             <CarouselItem key={index}>
                <Link href={img.href}>
-                <div className="relative w-full aspect-[16/9] md:h-[80vh] h-[50vh] bg-white">
+                <div className="relative w-full aspect-[16/9] md:h-[90vh] h-[50vh] bg-white">
                   <Image
                     src={img.src}
                     alt={img.alt}
